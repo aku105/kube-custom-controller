0e509413a3481031c83ec1feb2c4c620a698b76a
Amit Kumar <akumar105@sapient.com>
2018-02-16 12:10:04 +0000

    controller

--- apis/test/register.go
+++ apis/test/register.go
@@ -2,5 +2,5 @@ package test
 
 const (
 	// GroupName for export
-	GroupName = "test"
+	GroupName = "test.example.net"
 )
--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	azureRedisClientSet "github.com/amitkr0201/kube-custom-controller/client/clientset/versioned"
 	controller "github.com/amitkr0201/kube-custom-controller/controller"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	kuberconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	master      = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 )
 
 func main() {
-	controller.Execute()
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(*master, *kuberconfig)
+	if err != nil {
+		log.Printf("Error building kubeconfig: %v", err)
+	}
+
+	azureRedisClient, err := azureRedisClientSet.NewForConfig(cfg)
+	if err != nil {
+		log.Printf("Error building example clientset: %v", err)
+	}
+	kubeclient, _ := kubernetes.NewForConfig(cfg)
+	c := controller.NewController(azureRedisClient, kubeclient)
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	go c.Run(stopCh)
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGTERM)
+	signal.Notify(sigterm, syscall.SIGINT)
+	<-sigterm
 }
