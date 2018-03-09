26ffec7b53b1bf131179fc1b6b30ffc1492f5001
Amit Kumar <akumar105@sapient.com>
2018-03-09 15:11:57 +0000

    added codegen scripts

---
+++ hack/update-codegen.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+REPOSITORY="github.com/amitkr0201/kube-custom-controller"
+CONTROLLER_PACKAGE="azurerediscontroller"
+CONTROLLER_VERSION="v1alpha1"
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  ${REPOSITORY}/pkg/client ${REPOSITORY}/pkg/apis \
+  ${CONTROLLER_PACKAGE}:${CONTROLLER_VERSION} \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+
+# To use your own boilerplate text use:
+#   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
